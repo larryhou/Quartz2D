92ce7f2494d1d6735bd4eaf24d050d150d2aead5
larryhou <larryhou@foxmail.com>
2015-08-25 15:58:07 +0800

    Update

--- Quartz2D/ViewController.swift
+++ Quartz2D/ViewController.swift
@@ -76,7 +76,7 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         
-        let graph = ContextScaleView(frame: view.frame)
+        let graph = ShapeView(frame: view.frame)
         graph.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
         view.addSubview(graph)
     }
